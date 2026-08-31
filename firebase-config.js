15b1005239431d58228fabff9726a5e3463f9567
ooo610 <129843770+ooo610@users.noreply.github.com>
2026-08-31 13:40:54 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js';
+import { getFirestore, initializeFirestore } from 'https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCUkWudLdS-LbMOZY0zqvFm-Td73eaNjL4',
@@ -15,4 +15,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true
+});
