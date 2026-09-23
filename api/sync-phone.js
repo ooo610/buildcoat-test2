2ce4598b7e6be0f14ece4804e970f3117941fa8b
ooo610 <129843770+ooo610@users.noreply.github.com>
2026-09-23 22:19:16 +0300

    Add files via upload

--- api/sync-phone.js
+++ api/sync-phone.js
@@ -11,6 +11,17 @@ if (!admin.apps.length) {
 }
 
 export default async function handler(req, res) {
+
+  // CORS
+  res.setHeader('Access-Control-Allow-Origin', 'https://ooo610.github.io');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+
+  // Handle browser preflight request
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({
       error: 'Method not allowed'
